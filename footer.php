ce8cc9fa5f6083d22548a46a9c33dc93d6044891
Marc <marcvandalen@outlook.com>
2018-06-01 14:46:10 +0200

    .

---
+++ footer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marc
+ * Date: 1-6-2018
+ * Time: 08:59
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Marc van Dalen</title>
+</head>
+<footer>
+<div class="wrapper foot">
+    <ul class="footer-links-main">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Aliquam</a></li>
+        <li><a href="#">convallis</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+    <ul class="footer-cases">
+        <li><p>LATEST CASES</p></li>
+        <li><a href="#">LOREM - UT VENENATIS</a></li>
+        <li><a href="#">EXUPSUM - MALESUADA DIAM ORCI, EGET</a></li>
+        <li><a href="#">PRETIUM - SUSCIPIT VITAE</a></li>
+        <li><a href="#">FACILISIS - PELLENTESQUW LACINIA</a></li>
+    </ul>
+    <ul class="footer-apps">
+        <li><p>Apps</p></li>
+        <li><a href="#">Android</a></li>
+        <li><a href="#">Ios</a></li>
+        <li><a href="#">Windows</a></li>
+        <li><a href="#">Mac</a></li>
+    </ul>
+    <div class="footer-sm">
+        <a href="#">
+            <img src="images/youtubeicon.png" alt="youtube icon">
+        </a>
+        <a href="#">
+            <img src="images/facebookicon.png" alt="facebook icon">
+        </a>
+        <a href="#">
+            <img src="images/instagramicon.png" alt="instagram icon">
+        </a>
+    </div>
+    </footer>
